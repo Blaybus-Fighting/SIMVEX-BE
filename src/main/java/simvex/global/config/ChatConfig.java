aea4e92d11f6048c928cdea50f1fc163072ba3a5
Jeyong <dlwpdyd201@naver.com>
2026-02-06 02:09:01 +0900

    feat: ChatClient, Qdrant 관련 Config

---
+++ src/main/java/simvex/global/config/ChatConfig.java
@@ -0,0 +1,29 @@
+package simvex.global.config;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.rag.advisor.RetrievalAugmentationAdvisor;
+import org.springframework.ai.rag.retrieval.search.VectorStoreDocumentRetriever;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChatConfig {
+
+    @Bean
+    public ChatClient chatClient(ChatModel chatModel) {
+        return ChatClient.builder(chatModel).build();
+    }
+
+    @Bean
+    public RetrievalAugmentationAdvisor advisor(VectorStore vectorStore) {
+        return RetrievalAugmentationAdvisor.builder()
+                .documentRetriever(
+                        VectorStoreDocumentRetriever.builder()
+                                .vectorStore(vectorStore)
+                                .build()
+                )
+                .build();
+    }
+}
