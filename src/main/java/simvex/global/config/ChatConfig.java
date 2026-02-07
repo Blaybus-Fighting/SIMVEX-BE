4e643064ca97dcc4ba6c62420ba09c20cb75fcd1
Jeyong <dlwpdyd201@naver.com>
2026-02-07 16:29:02 +0900

    feat: ChatClient 기본 시스템 프롬포트 적용

--- src/main/java/simvex/global/config/ChatConfig.java
+++ src/main/java/simvex/global/config/ChatConfig.java
@@ -1,19 +1,41 @@
 package simvex.global.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.rag.advisor.RetrievalAugmentationAdvisor;
 import org.springframework.ai.rag.retrieval.search.VectorStoreDocumentRetriever;
 import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import simvex.global.exception.CustomException;
+import simvex.global.exception.ErrorCode;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@Slf4j
 @Configuration
 public class ChatConfig {
 
+    private final ResourceLoader resourceLoader;
+
+    @Value("${ai.prompt.system-path}")
+    private String systemPromptsPath;
+
+    public ChatConfig(ResourceLoader resourceLoader) {
+        this.resourceLoader = resourceLoader;
+    }
+
     @Bean
     public ChatClient chatClient(ChatModel chatModel) {
-        return ChatClient.builder(chatModel).build();
+        String systemPrompt = resolveSystemPrompt();
+        return ChatClient.builder(chatModel)
+                .defaultSystem(s -> s.text(systemPrompt))
+                .build();
     }
 
     @Bean
@@ -25,5 +47,22 @@ public class ChatConfig {
                                 .build()
                 )
                 .build();
+    }
+
+    private String resolveSystemPrompt() {
+        if (systemPromptsPath != null) {
+            try {
+                Resource resource = resourceLoader.getResource(systemPromptsPath);
+                if (resource.exists()) {
+                    byte[] bytes = resource.getInputStream().readAllBytes();
+                    return new String(bytes, StandardCharsets.UTF_8);
+                }
+            } catch (IOException e) {
+                log.error("Resoure 조회 에러! : {}", e.getMessage());
+                throw new CustomException(ErrorCode.RESOURCE_NOT_FOUND);
+            }
+        }
+
+        return "";
     }
 }
