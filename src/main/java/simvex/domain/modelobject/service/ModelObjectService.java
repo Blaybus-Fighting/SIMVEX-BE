7b8b755acf5c717f4dfbeb74c084faa4ba64b72a
yangyanghyunjung <hyunjungyang0922@gmail.com>
2026-02-09 17:46:31 +0900

    refactor : 불필요한 import 제거 #74

--- src/main/java/simvex/domain/modelobject/service/ModelObjectService.java
+++ src/main/java/simvex/domain/modelobject/service/ModelObjectService.java
@@ -2,7 +2,6 @@ package simvex.domain.modelobject.service;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import simvex.domain.modelobject.dto.ModelObjectResponse;
