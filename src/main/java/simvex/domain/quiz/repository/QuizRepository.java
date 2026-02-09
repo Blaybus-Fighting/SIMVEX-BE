684e470cb8f8463e33ceaeaa90110e996e040bc5
조원희 <woni0517@naver.com>
2026-02-09 15:31:49 +0900

    refactor: Quiz 서비스 로직 변경

--- src/main/java/simvex/domain/quiz/repository/QuizRepository.java
+++ src/main/java/simvex/domain/quiz/repository/QuizRepository.java
@@ -5,16 +5,10 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import simvex.domain.quiz.entity.Quiz;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface QuizRepository extends JpaRepository<Quiz, Long> {
 
-    @Query(value = """
-            SELECT *
-            FROM quiz
-            WHERE model_id = :modelId
-            ORDER BY RANDOM()
-            LIMIT 1
-            """, nativeQuery = true)
-    Optional<Quiz> findRandomByModelId(@Param("modelId") Long modelId);
+    List<Quiz> findAllByModelId(Long modelId);
 }
